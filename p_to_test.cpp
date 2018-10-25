658475fadb4e3a23d08d819a3a5cfd5100a83f8b
PaulRitaldato <paulritaldato001@ufl.edu>
2018-10-25 16:29:31 -0400

    testing

--- p_to_test.cpp
+++ p_to_test.cpp
@@ -4,5 +4,4 @@ int main(){
     int x;
     std::cin >> x;
     std::cout << x*2;
-    std::cout << "From p_to_test"
 }
--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 mkfifo test_pipe
+./tests < test_pipe | ./p_to_test >test_pipe
 
-./tests | ./p_to_test
+echo $test_pipe
 
 rm test_pipe
